6876020f39bb9406e659b40f192cc3ee69dcc088
Liren He <hlr7999@gmail.com>
2019-09-17 20:06:14 +0800

    problem 78 (bit manipulation)

--- src/78.cpp
+++ src/78.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    // backtracking
+    // Approch1 backtracking
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
         vector<int> temp;
@@ -18,5 +18,23 @@ public:
             backtrack(result, nums, temp, i + 1);
             temp.pop_back();
         }
+    }
+    // Approch2 bit manipulation
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        int ans_num = 1 << n;
+        for (int i = 0; i < ans_num; ++i) {
+            vector<int> temp;
+            int bit = 1;
+            for (int index = 0; index < n; ++index) {
+                if (bit & i) {
+                    temp.push_back(nums[index]);
+                }
+                bit = bit << 1;
+            }
+            result.push_back(temp);
+        }
+        return result;
     }
 };
