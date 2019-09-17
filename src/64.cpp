91d729c055b1ccd116ef2bfbc2c0ca2e0fb4bfbc
Liren He <hlr7999@gmail.com>
2019-09-17 10:57:05 +0800

    problem 64

---
+++ src/64.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    // Dynamic Programming
+    int minPathSum(vector<vector<int>>& grid) {
+        // use grid space
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j-1];
+        }
+        for (int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i-1][0];
+            for (int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
