2fdd90839582e21f79044d5daa406989d88df06f
Liren He <hlr7999@gmail.com>
2019-09-18 23:35:11 +0800

    problem 84

--- src/84.cpp
+++ src/84.cpp
@@ -1,10 +1,44 @@
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
+    // for each bar, find the maximum area that contains the bar
+    // and find the maximum of these maximum areas
+    // to find the maximum area, we find the first bar that
+    // less then the current bar on the right and left
     int largestRectangleArea(vector<int>& heights) {
-        
+        stack<int> st;
+        int cur = 0;
+        int max_area = 0;
+        while (cur < heights.size()) {
+            if (st.empty()) {
+                st.push(cur);
+                ++cur;
+            } else if (heights[cur] >= heights[st.top()]) {
+                st.push(cur);
+                ++cur;
+            } else {
+                int top_height = heights[st.top()];
+                st.pop();
+                int left_less = st.empty() ? -1 : st.top();
+                int area = (cur - left_less - 1) * top_height;
+                if (area > max_area) {
+                    max_area = area;
+                }
+            }
+        }
+        while (!st.empty()) {
+            int top_height = heights[st.top()];
+            st.pop();
+            int left_less = st.empty() ? -1 : st.top();
+            int area = (cur - left_less - 1) * top_height;
+            if (area > max_area) {
+                max_area = area;
+            }
+        }
+        return max_area;
     }
 };
