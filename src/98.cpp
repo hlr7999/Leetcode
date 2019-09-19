be62584e8cec99bf4c9a62a7f8446c2aa7a9a5fd
Liren He <hlr7999@gmail.com>
2019-09-19 17:45:25 +0800

    fix

---
+++ src/98.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        
+    }
+};
