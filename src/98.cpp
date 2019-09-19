520401392f952b8e2e676e27a0c662364a87ec15
Liren He <hlr7999@gmail.com>
2019-09-19 18:36:58 +0800

    problem 98

--- src/98.cpp
+++ src/98.cpp
@@ -1,4 +1,6 @@
-#include <stdlib.h>
+#include <stack>
+
+using namespace std;
 
 struct TreeNode {
     int val;
@@ -9,7 +11,25 @@ struct TreeNode {
 
 class Solution {
 public:
+    // inordee traversal, and the list should be ordered
     bool isValidBST(TreeNode* root) {
-        
+        stack<TreeNode*> st;
+        int pre = INT_MIN;
+        bool first = true;
+        while (!st.empty() || root) {
+            while (root) {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            if (!first && root->val <= pre) {
+                return false;
+            }
+            pre = root->val;
+            first = true;
+            root = root->right;
+        }
+        return true;
     }
 };
