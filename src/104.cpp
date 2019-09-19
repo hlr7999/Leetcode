0604d0cb1f3b1ecf53fa982b7bcff0b8dfcf2d76
Liren He <hlr7999@gmail.com>
2019-09-19 20:13:10 +0800

    problem 104

---
+++ src/104.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root ? max(maxDepth(root->left), maxDepth(root->right)) + 1 : 0;
+    }
+};
