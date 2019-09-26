68007cda6fab6e74e608cfacdf68a5c69ac8cf80
Liren He <hlr7999@gmail.com>
2019-09-26 20:25:08 +0800

    problem 221

--- src/221.cpp
+++ src/221.cpp
@@ -22,7 +22,7 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
-                if (matrix[i][j] == '1') {
+                if (matrix[i-1][j-1] == '1') {
                     dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                     if (dp[i][j] > max_len) max_len = dp[i][j];
                 }
