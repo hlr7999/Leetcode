32c6ec655d736499d6c03ebfbf5de89ad7e8f615
Liren He <hlr7999@gmail.com>
2019-09-26 20:24:58 +0800

    problem 221

---
+++ src/221.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+// speed up cin and cout
+static int __ = []() { 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    // Dynamic programming
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        if (n == 0) return 0;
+        int m = matrix[0].size();
+        int max_len = 0;
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    if (dp[i][j] > max_len) max_len = dp[i][j];
+                }
+            }
+        }
+        return max_len*max_len;
+    }
+};
