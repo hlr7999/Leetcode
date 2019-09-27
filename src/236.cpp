4a6ef6242aa238751dcd06c9b3326e5999b16005
Liren He <hlr7999@gmail.com>
2019-09-27 23:45:03 +0800

    problem 236

---
+++ src/236.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+// speed up cin and cout
+static int __ = []() { 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    return 0;
+}();
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // recursive
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q)
+            return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        else if (left) return left;
+        else return right;
+    }
+};
