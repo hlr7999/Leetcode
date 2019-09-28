47e3c8b11f7bbdf2b1e6c67aed38c04b8e051bb5
Liren He <hlr7999@gmail.com>
2019-09-28 22:38:16 +0800

    problem 287

---
+++ src/287.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// speed up cin and cout
+static int __ = []() { 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    return 0;
+}();
+
+
+// Floyd's Tortoise and Hare (Cycle Detection)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
