b8a263a9be85b88be308615543c6208545d54fea
Liren He <hlr7999@gmail.com>
2019-10-01 21:33:37 +0800

    problem 338

---
+++ src/338.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// speed up cin and cout
+static int __ = []() { 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    // dp
+    vector<int> countBits(int num) {
+        vector<int> res(num + 1, 0);
+        for (int i = 1; i <= num; ++i) {
+            res[i] = res[i>>1] + (i&1);
+        }
+        return res;
+    }
+};
