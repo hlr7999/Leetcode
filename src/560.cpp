2c5069302333e20b6d3fe97dddf001e9c5406a0f
Liren He <hlr7999@gmail.com>
2019-10-06 13:01:48 +0800

    peoblem 560

---
+++ src/560.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    // cumulative sum
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        for (int num : nums) {
+            sum += num;
+            if (mp[sum-k])
+                count += mp[sum-k];
+            ++mp[sum];
+        }
+        return count;
+    }
+};
