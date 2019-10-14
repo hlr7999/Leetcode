0c682c636480fe481595a48a3ccfad5cbce81e52
Liren He <hlr7999@gmail.com>
2019-10-14 11:28:51 +0800

    problem 171

---
+++ src/171.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int ans = 0;
+        for (char c : s) {
+            ans = ans*26 + (c - 'A' + 1);
+        }
+        return ans;
+    }
+};
