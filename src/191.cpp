eddc79226d30aeb0537fcab440d1ea14e72236af
Liren He <hlr7999@gmail.com>
2019-10-15 20:11:34 +0800

    problem 191

---
+++ src/191.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while (n) {
+            res += (n&1);
+            n >>= 1;
+        }
+        return res;
+    }
+};
