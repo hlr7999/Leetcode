9cd80c07cff040a82212cdca2e70edda04ae1902
Liren He <hlr7999@gmail.com>
2019-10-24 15:20:37 +0800

    problem 380

---
+++ src/380.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+static int __ = []() {
+   std::ios::sync_with_stdio(false);
+   std::cin.tie(nullptr);
+   std::cout.tie(nullptr);
+   return 0;
+}();
+
+class RandomizedSet {
+private:
+    vector<int> nums;
+    unordered_map<int, int> mp;
+
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (mp.find(val) != mp.end())
+            return false;
+        nums.emplace_back(val);
+        mp[val] = nums.size() - 1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (mp.find(val) == mp.end())
+            return false;
+        int last = nums.back();
+        mp[last] = mp[val];
+        nums[mp[val]] = last;
+        nums.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
