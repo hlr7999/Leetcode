fe49a4440d063a88dbb489a336529b8451734ec3
Liren He <hlr7999@gmail.com>
2019-11-05 14:27:08 +0800

    problem 27

---
+++ src/27.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for (int num : nums) {
+            if (num != val) {
+                nums[index++] = num;
+            }
+        }
+        return index;
+    }
+};
