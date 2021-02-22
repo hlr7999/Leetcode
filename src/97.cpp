4f22acd8be098e331588c6bc427452df58487b59
hlr7999 <hlr7999@outlook.com>
2021-02-22 18:34:11 +0800

    problem 97

---
+++ src/97.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s3.size() != s1.size() + s2.size())
+            return false;
+        vector<bool> dp(s2.size()+1, false);
+        for (int i = 0; i <= s1.size(); ++i) {
+            for (int j = 0; j <= s2.size(); ++j) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j-1] && (s2[j-1] == s3[j-1]);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && (s1[i-1] == s3[i-1]);
+                } else {
+                    dp[j] = (dp[j] && (s1[i-1] == s3[i+j-1])) || 
+                        (dp[j-1] && (s2[j-1] == s3[i+j-1]));
+                }
+            }
+        }
+        return dp[s2.size()];
+    }
+};
